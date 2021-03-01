dfe6aef400e61531fe8d0ec980b97bb9c13e9ce2
YY <yuzhiyang@52tt.com>
2021-03-01 16:25:02 +0800

    调整公用数据

--- SoundDriftingBottle/Common/CommonData.swift
+++ SoundDriftingBottle/Common/CommonData.swift
@@ -12,7 +12,15 @@ let isIPhoneX: Bool = GKConfigure.gk_isNotchedScreen()
 let kScreenW = UIScreen.main.bounds.size.width
 let kScreenH = UIScreen.main.bounds.size.height
 
+
 let kStatusBar_Height: CGFloat = GKConfigure.gk_statusBarFrame().size.height
 let kNavBar_Height: CGFloat = kStatusBar_Height + 44.0
 
 let ADAPTATIONRATIO = kScreenW / 750.0
+
+//音频波形图
+let columnNumber = 50
+let columnWidth = (kScreenW / 50) / 2
+let noVoice = -46.0     // 该值代表低于-46.0的声音都认为无声音
+let maxVolume = 55.0    // 该值代表最高声音为55.0
+
